46575d4fec220625129b5e7ca548f3a0b03b3771
Laurynas Karvelis <laurynas.karvelis@gmail.com>
2012-05-12 20:40:17 +0100

    Added author and license for this project

--- library/Facebook/Api.php
+++ library/Facebook/Api.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @author  Laurynas Karvelis <laurynas.karvelis@gmail.com>
+ * @author  Explosive Brains Limited
+ * @license http://sam.zoy.org/wtfpl/COPYING
+ */
+
 class Facebook_Api extends Facebook_Api_Base
 {
     /**
--- library/Facebook/Auth/Adapter/Exception.php
+++ library/Facebook/Auth/Adapter/Exception.php
@@ -1,5 +1,11 @@
 <?php
 
+/**
+ * @author  Laurynas Karvelis <laurynas.karvelis@gmail.com>
+ * @author  Explosive Brains Limited
+ * @license http://sam.zoy.org/wtfpl/COPYING
+ */
+
 class Facebook_Auth_Adapter_Exception extends Exception
 {
 
